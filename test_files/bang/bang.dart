0aa44915bf0ba09409bdeb2fd9f5ef31626e8884
Robin Combrink <robin.combrink@gmail.com>
2025-02-19 19:31:34 +0200

    Add multiple bang operators to a single file

--- test_files/bang/bang.dart
+++ test_files/bang/bang.dart
@@ -2,6 +2,10 @@ void main() {
   String? string = maybeString("abc");
   String number = string! + "a";
   print(number);
+
+  String? string1 = maybeString("abc");
+  String number2 = string1! + "a";
+  print(number2);
 }
 
 String? maybeString(String? string) {
