09bafbfc4a8a697c734b650542bb8afbec724bc1
Robin Combrink <robin.combrink@gmail.com>
2025-02-21 22:21:29 +0200

    Simplify the match inside analyse_file when returning the analysis

--- src/main.rs
+++ src/main.rs
@@ -9,13 +9,7 @@ fn main() -> Result<FailureOutput> {
     let mut failure_finder = FailureFinder::default();
 
     let failures = match args {
-        NodeAnalyser::File(args) => {
-            let analysis = failure_finder.analyse_file(args.file_path);
-            match analysis {
-                Ok(analysis) => Ok(vec![analysis]),
-                Err(err) => Err(err),
-            }
-        }
+        NodeAnalyser::File(args) => Ok(vec![failure_finder.analyse_file(args.file_path)?]),
         NodeAnalyser::Files(args) => failure_finder.analyse_files(args.file_paths),
         NodeAnalyser::Directory(args) => failure_finder.analyse_directory(args.directory_path),
     };
